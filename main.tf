9e6aa26401246579fdf6fa5373204db105df0d11
moxDroid Labs Inc <pritam.world@gmail.com>
2023-07-25 18:07:41 -0400

    company name updated

--- main.tf
+++ main.tf
@@ -2,36 +2,36 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "lambton" {
-  name     = "lambton-resource-group"
+resource "azurerm_resource_group" "moxdroid" {
+  name     = "moxdroid-resource-group"
   location = "West Europe"
 }
 
-resource "azurerm_virtual_network" "lambton" {
-  name                = "lambton-vnet"
+resource "azurerm_virtual_network" "moxdroid" {
+  name                = "moxdroid-vnet"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.lambton.location
-  resource_group_name = azurerm_resource_group.lambton.name
+  location            = azurerm_resource_group.moxdroid.location
+  resource_group_name = azurerm_resource_group.moxdroid.name
 }
 
-resource "azurerm_subnet" "lambton" {
-  name                 = "lambton-subnet"
-  resource_group_name  = azurerm_resource_group.lambton.name
-  virtual_network_name = azurerm_virtual_network.lambton.name
+resource "azurerm_subnet" "moxdroid" {
+  name                 = "moxdroid-subnet"
+  resource_group_name  = azurerm_resource_group.moxdroid.name
+  virtual_network_name = azurerm_virtual_network.moxdroid.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 
-resource "azurerm_public_ip" "lambton" {
-  name                = "lambton-publicip"
-  location            = azurerm_resource_group.lambton.location
-  resource_group_name = azurerm_resource_group.lambton.name
+resource "azurerm_public_ip" "moxdroid" {
+  name                = "moxdroid-publicip"
+  location            = azurerm_resource_group.moxdroid.location
+  resource_group_name = azurerm_resource_group.moxdroid.name
   allocation_method   = "Static"
 }
 
-resource "azurerm_network_security_group" "lambton" {
-  name                = "lambton-nsg"
-  location            = azurerm_resource_group.lambton.location
-  resource_group_name = azurerm_resource_group.lambton.name
+resource "azurerm_network_security_group" "moxdroid" {
+  name                = "moxdroid-nsg"
+  location            = azurerm_resource_group.moxdroid.location
+  resource_group_name = azurerm_resource_group.moxdroid.name
 
   security_rule {
     name                       = "allow_ssh"
@@ -58,30 +58,30 @@ resource "azurerm_network_security_group" "lambton" {
   }
 }
 
-resource "azurerm_network_interface" "lambton" {
-  name                = "lambton-nic"
-  location            = azurerm_resource_group.lambton.location
-  resource_group_name = azurerm_resource_group.lambton.name
+resource "azurerm_network_interface" "moxdroid" {
+  name                = "moxdroid-nic"
+  location            = azurerm_resource_group.moxdroid.location
+  resource_group_name = azurerm_resource_group.moxdroid.name
 
   ip_configuration {
-    name                          = "lambton-ipconfig"
-    subnet_id                     = azurerm_subnet.lambton.id
+    name                          = "moxdroid-ipconfig"
+    subnet_id                     = azurerm_subnet.moxdroid.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.lambton.id
+    public_ip_address_id          = azurerm_public_ip.moxdroid.id
   }
 
 }
 
-resource "azurerm_network_interface_security_group_association" "lambton" {
-  network_interface_id      = azurerm_network_interface.lambton.id
-  network_security_group_id = azurerm_network_security_group.lambton.id
+resource "azurerm_network_interface_security_group_association" "moxdroid" {
+  network_interface_id      = azurerm_network_interface.moxdroid.id
+  network_security_group_id = azurerm_network_security_group.moxdroid.id
 }
 
-resource "azurerm_virtual_machine" "lambton" {
-  name                  = "lambton-vm"
-  location              = azurerm_resource_group.lambton.location
-  resource_group_name   = azurerm_resource_group.lambton.name
-  network_interface_ids = [azurerm_network_interface.lambton.id]
+resource "azurerm_virtual_machine" "moxdroid" {
+  name                  = "moxdroid-vm"
+  location              = azurerm_resource_group.moxdroid.location
+  resource_group_name   = azurerm_resource_group.moxdroid.name
+  network_interface_ids = [azurerm_network_interface.moxdroid.id]
   vm_size               = "Standard_DS1_v2"
 
   storage_image_reference {
@@ -92,14 +92,14 @@ resource "azurerm_virtual_machine" "lambton" {
   }
 
   storage_os_disk {
-    name              = "lambton-osdisk"
+    name              = "moxdroid-osdisk"
     caching           = "ReadWrite"
     create_option    = "FromImage"
     managed_disk_type = "Standard_LRS"
   }
 
   os_profile {
-    computer_name  = "lambtonvm"
+    computer_name  = "moxdroidvm"
     admin_username = "adminuser"
     admin_password = "adminpasswordG3#"
   }
@@ -110,5 +110,5 @@ resource "azurerm_virtual_machine" "lambton" {
 }
 
 output "instance_ip" {
-  value = azurerm_public_ip.lambton.ip_address
+  value = azurerm_public_ip.moxdroid.ip_address
 }
